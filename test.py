a6a56b358178cd0a3377c838157d7570ad09bc41
laramiedunlap <laramiedunlap@gmail.com>
2022-07-20 08:21:10 -0400

    saving current work

--- test.py
+++ test.py
@@ -1,6 +1,12 @@
 from requests import options
-from utils import Service, Trade, init_stock_trades
+from utils import Service, Trade, init_stock_trades, init_options_trades
 import streamlit as st
+import pandas as pd
+
+
+
+
+
 
 service = st.selectbox(
     "Which service would you like?",
@@ -9,11 +15,34 @@ service = st.selectbox(
 
 if service != 'Select':
     st.write(f"You selected: {service}")
-    user_service = Service(name=service, KPI= {}, open_trades={})
+    @st.cache(allow_output_mutation=True) 
+    def user_service_set(name):
+        return Service(name=name, KPI= {}, open_trades={})
+    user_service = user_service_set(service) 
     user_action = st.selectbox(label = "What would you like to do?",options =["Select","Input New Open Trades", "View Open Trades", "Close Open Trades"])
 
+
     if user_action == "Input New Open Trades":
-        stocks = st.text_input("copy and paste tickers from the Tracker")
-        init_stock_trades(user_service, stocks)
-        for k in user_service.open_trades.keys():
-            st.write(user_service.open_trades[k].entry_price)
+        st.write("---")
+        st.write("## First, let's handle the stock trades")
+        st.write("---")
+        st.write("### Please select the following columns in this order from the tracker: ")
+        st.write("#### Symbol, Long/Short, Entry Price")
+
+        stocks = st.text_input("copy and paste from the Tracker.")
+        if st.button("add stock trades"):
+            init_stock_trades(user_service, stocks)
+            st.write(user_service.open_trades.keys())
+        st.write("---")    
+        st.write("## Second, let's handle the options trades")
+        st.write("---")
+        st.write("### Please select the following columns in this order from the tracker: ")
+        st.write("#### Symbol, Option Data, Entry Price")
+            
+        options = st.text_input("copy and paste options data from the Tracker.")
+        if st.button("add stock options"):
+            init_options_trades(user_service, options)
+            st.write(user_service.open_trades.keys())
+
+        for value in user_service.open_trades.values():
+            st.write(value.symbol , value.status, value.entry_price, value.is_option, value.expiration)
--- utils.py
+++ utils.py
@@ -6,13 +6,11 @@ import numpy as np
 import datetime as dt
 from datetime import datetime
 
-
-
 class Service:
     def __init__(self, name, KPI, open_trades):
         self.name = name
         self.KPI = {}
-        self.open_trades = {}
+        self.open_trades = open_trades
 
 class Trade:
     def __init__(self, service_name, status, symbol, long_short, size, is_option, entry_price):
@@ -21,32 +19,64 @@ class Trade:
         self.symbol = symbol
         self.long_short = long_short
         self.size = 1
+        self.entry_price = entry_price   
         self.is_option = is_option
-        if str(status).capitalize() == 'OPEN':
-            self.entry_date = dt.datetime.now().strftime('%m/%d/%Y')
-        else:
-            self.entry_date = np.NaN
-        self.entry_price = entry_price
-        self.exit_date = np.NaN
-       
-        self.exit_price = np.array([])
-        if is_option:
-            self.expiration = np.NaN
-            self.strike = np.NaN
-            self.opt_type = np.NaN
 
-def init_stock_trades(service, ticker_string):
+    def set_opt_details(self, expry, strike, opt_tp):
+        if self.is_option:
+            self.expiration = expry
+            self.strike = strike
+            self.opt_type = opt_tp
+        else:
+            self.expiration = expry
+            self.strike = strike
+            self.opt_type = opt_tp
+
+    def set_current_price(self):
+        self.current_price = round(float(yf.Ticker(self.symbol).history(period="1d")['Close']),2)
+
+
+def init_stock_trades(service, user_string):
     """Takes a string copied from google sheets and creates trade objects inside of a Service"""
-    user_list = re.findall(r'\S+\s\S+\s\S+', ticker_string)
+    user_list = re.findall(r'\S+\s\S+\s\S+', user_string)
     user_list = [x.replace('\t',';') for x in user_list]
     for elem in user_list:
         trd_details = elem.split(';')
         tk = trd_details[0]
         l_s = trd_details[1]
         e_p = trd_details[2][1:]
-        service.open_trades[trd_details[0]] = Trade(service_name=service.name, status= "open",symbol=tk,long_short=l_s,size=1,is_option=False,entry_price=float(e_p))
+        expry = None
+        strike = None
+        opt_tp = None
+
+        service.open_trades[tk] = Trade(service_name=service.name, status= "open", symbol=tk, long_short=l_s, size=1, entry_price=float(e_p), is_option = False)
+        service.open_trades[tk].set_opt_details(expry, strike, opt_tp)
+
+        service.open_trades[tk].set_current_price()
+
     return None
 
-def update_long_short(service, long_short_str):
-    """Takes a string of long short statuses from google sheets and assigns them to their respective trades"""
+def init_options_trades(service, user_string):
+    """Takes a string copied from google sheets and creates options trade objects inside of a Service"""
+    user_list = user_list = re.findall(r'\S+\s\S+\s\S+\s\S+', user_string)
+    user_list = [x.replace('\t',';') for x in user_list]
+    for elem in user_list:
+        trd_details = elem.split(';')
+        tk = trd_details[0]
+        l_s = "Long"
+        e_p = trd_details[2][1:]
+
+        expry = trd_details[1].split(' ')[0]
+        strike = float(trd_details[1].split(' ')[1][1:])
+
+        opt_tp = str(trd_details[1].split(' ')[1][0])
+
+        if opt_tp.capitalize() == "C":
+            opt_tp = "Calls"
+        else:
+            opt_tp = "Puts"
+
+        service.open_trades[f"{tk}--{opt_tp}"] = Trade(service_name=service.name, status= "open",symbol=tk,long_short=l_s,size=1, entry_price=float(e_p), is_option= True)
+        service.open_trades[f"{tk}--{opt_tp}"].set_opt_details(expry, strike, opt_tp)
     return None
+
